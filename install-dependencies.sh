675c0773cc6fda3c883c04a81305e4b453282dc5
Don Murray <dmurray@dmurray.ca>
2024-07-12 14:13:04 +0000

    install ubuntu deps

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Is task already installed?
-which task
-rc=$?
+packages="openssh-client sudo git bats"
 
-set -e 
+apt update && \
+    apt upgrade -y && \
+    apt install ${packages} -y
+
+exit
+
 if [ 0 -ne $rc ]; then
     sudo bash -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin v3.38.0 
 fi
